b5a7ec229f284a365f4d26a7c200dea9d9f5034d
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-10-05 20:42:56 -0400

    my sample data

---
+++ src/scripts/debts.js
@@ -0,0 +1,116 @@
+module.exports = {
+  debt01: {
+    creditor: 'Reedy Family Medicine',
+    monthly: 30,
+    previousBalance: 116.20,
+    currentBalance: 116.20
+  },
+  debt02: {
+    creditor: 'GHS',
+    monthly: 30,
+    previousBalance: 367.18,
+    currentBalance: 367.18
+  },
+  debt03: {
+    creditor: 'GHS',
+    monthly: 30,
+    previousBalance: 402,
+    currentBalance: 402
+  },
+  debt04: {
+    creditor: 'Bon Secour',
+    monthly: 30,
+    previousBalance: 796.06,
+    currentBalance: 796.06
+  },
+  debt05: {
+    creditor: 'Anmed',
+    monthly: 30,
+    previousBalance: 874.73,
+    currentBalance: 874.73
+  },
+  debt06: {
+    creditor: 'Synchrony',
+    monthly: 240,
+    previousBalance: 963.10,
+    currentBalance: 963.10
+  },
+  debt07: {
+    creditor: 'USAA',
+    monthly: 275.64,
+    previousBalance: 1098.83,
+    currentBalance: 1098.83
+  },
+  debt08: {
+    creditor: 'GHS',
+    monthly: 30,
+    previousBalance: 1395.37,
+    currentBalance: 1395.37
+  },
+  debt09: {
+    creditor: 'Navient',
+    monthly: 223.69,
+    previousBalance: 1513.94,
+    currentBalance: 1513.94
+  },
+  debt10: {
+    creditor: 'GHS',
+    monthly: 30,
+    previousBalance: 1732.97,
+    currentBalance: 1732.97
+  },
+  debt11: {
+    creditor: 'ECSI',
+    monthly: 42.43,
+    previousBalance: 2443.32,
+    currentBalance: 2443.32
+  },
+  debt12: {
+    creditor: 'American Express – Blue Cash',
+    monthly: 35,
+    previousBalance: 2495.40,
+    currentBalance: 2495.40
+  },
+  debt13: {
+    creditor: 'Lending Club – 46682270',
+    monthly: 320,
+    previousBalance: 5030.22,
+    currentBalance: 5030.22
+  },
+  debt14: {
+    creditor: 'Nelnet',
+    monthly: 149,
+    previousBalance: 6100.31,
+    currentBalance: 6100.31
+  },
+  debt15: {
+    creditor: 'Discover',
+    monthly: 120,
+    previousBalance: 6781.94,
+    currentBalance: 6781.94
+  },
+  debt16: {
+    creditor: 'American Express – Delta',
+    monthly: 216,
+    previousBalance: 9000,
+    currentBalance: 8867.28
+  },
+  debt17: {
+    creditor: 'Lending Club – 70801753',
+    monthly: 337.16,
+    previousBalance: 12035.11,
+    currentBalance: 12035.11
+  },
+  debt18: {
+    creditor: 'SCSL',
+    monthly: 121,
+    previousBalance: 17414.41,
+    currentBalance: 17414.41
+  },
+  debt19: {
+    creditor: 'FedLoan',
+    monthly: 270,
+    previousBalance: 22033.37,
+    currentBalance: 22033.37
+  }
+};
