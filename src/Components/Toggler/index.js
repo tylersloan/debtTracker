d16d18d0bd4203700b7e24defce4457aba4db0ba
Tyler Sloan <tyler.g.sloan@gmail.com>
2016-10-06 13:38:19 -0400

    refactor toggler

--- src/Components/Toggler/index.js
+++ src/Components/Toggler/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 import styles from './Toggler.css';
-import form from '../../styles/Form.css';
 
 class Toggler extends React.Component {
   render() {
     return (
-      <label className={`${form.checkbox} ${styles.toggler}`}>
-       <span className={form.label}>Edit Mode</span>
+      <label className={styles.toggler}>
+       <span className={styles.label}>Edit Mode</span>
        <input onChange={(e) => this.props.toggleView(e)} type="checkbox" />
-       <span className={form.switch}></span>
+       <span className={`${styles.switch} ${styles.switch}`}></span>
       </label>
     )
   }
